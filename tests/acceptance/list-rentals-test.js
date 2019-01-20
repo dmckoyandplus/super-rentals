4f75fd22b118ab21f63ff97ada65256e604062a0
dmckoyandplus <dmckoy@andplus.com>
2019-01-20 07:54:39 -0500

    modified app test to use ember data

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,10 +1,12 @@
 import { module, test } from 'qunit';
 import { click, currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
-
+  setupMirage(hooks);
+  
   test('should show rentals as the home page', async function(assert){
     await visit('/');
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
